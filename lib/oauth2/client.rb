b983ed24c8faa3d6ba21618402cf3acc8264bf5b
Jason Stillwell <jstillwell@zendesk.com>
2016-03-31 13:29:19 -0700

    .

--- lib/oauth2/client.rb
+++ lib/oauth2/client.rb
@@ -89,6 +89,7 @@ module OAuth2
       connection.response :logger if ENV['OAUTH_DEBUG'] == 'true'
 
       url = connection.build_url(url, opts[:params]).to_s
+      puts url
 
       response = connection.run_request(verb, url, opts[:body], opts[:headers]) do |req|
         yield(req) if block_given?
